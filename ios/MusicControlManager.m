bc2ef3bbe6e021a03c6bc872e69bf75773bd639b
jeffbonasso-ambifi <jeff.bonasso@ambifi.com>
2019-05-30 16:32:43 -0400

    Patch for allowing play and record

--- ios/MusicControlManager.m
+++ ios/MusicControlManager.m
@@ -178,9 +178,10 @@ RCT_EXPORT_METHOD(enableControl:(NSString *) controlName enabled:(BOOL) enabled 
 /* We need to set the category to allow remote control etc... */
 
 RCT_EXPORT_METHOD(enableBackgroundMode:(BOOL) enabled){
-    AVAudioSession *session = [AVAudioSession sharedInstance];
-    [session setCategory: AVAudioSessionCategoryPlayback error: nil];
-    [session setActive: enabled error: nil];
+     AVAudioSession *session = [AVAudioSession sharedInstance];
+     [session setCategory: AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker|AVAudioSessionCategoryOptionAllowBluetooth error: nil];
+//    [session setCategory: AVAudioSessionCategoryPlayback error: nil];
+     [session setActive: enabled error: nil];
 }
 
 RCT_EXPORT_METHOD(stopControl){
